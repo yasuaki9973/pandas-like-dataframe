754856f62876a86330d0bedb0556c7774685ecf8
yasuaki9973 <estpolis1127@gmail.com>
2020-08-23 02:01:06 +0900

    addの暫定版追加

--- core/Series.py
+++ core/Series.py
@@ -5,6 +5,20 @@ class Series:
     def __init__(self, values):
         self.__values = values
 
+    def __add__(self, other):
+
+        if not isinstance(other, Series):
+            other = Series([other] * len(self.__values))
+
+        return Series([value1 + value2 for value1, value2 in zip(self.__values, other.__values)])
+
+    def __radd__(self, other):
+
+        if not isinstance(other, Series):
+            other = Series([other] * len(self.__values))
+
+        return Series([value1 + value2 for value1, value2 in zip(other.__values, self.__values)])
+
     def __repr__(self):
         return '{}'.format(self.__values)
 
